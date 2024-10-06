525eff784d875841bd9faff3345325b119d35114
Kira <lwallker37@gmail.com>
2024-10-07 01:14:29 +0300

    ss

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main ()
 {
- cout << "Hello world Добавление новой строчки или типо того, и еще одно добавление, и еще однaф" << endl;
+ cout << "ghbdtn vbh" << endl;
  return 0;
 }
